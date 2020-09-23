d06e9826f05fd236b8a6a910b7531667ca9a066d
tatsuomi <tatsu20000303@gmail.com>
2020-09-24 04:58:10 +0900

    Initial Commit

---
+++ secondlit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondlit
+//
+//  Created by tatsuomi kikawa on 2020/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
