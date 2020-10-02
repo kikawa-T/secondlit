7dc935462cf14eddb9c4f2b109cb40eec2d7fc6a
tatsuomi <tatsu20000303@gmail.com>
2020-10-03 08:20:47 +0900

    SwiftPacageから入れたLTMorphingLabelがうまく使用できない．

--- secondlit/forthViewController.swift
+++ secondlit/forthViewController.swift
@@ -13,6 +13,8 @@ class forthViewController: UIViewController {
     
     @IBOutlet var text1: UILabel!
     
+    @IBOutlet var text2: UILabel!
+    
     //@IBOutlet var text2: UILabel!
     
     //飼っているbutton
@@ -23,21 +25,26 @@ class forthViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        thanks.text = String("見てくれてありがとうございます．")
-        text1.text = String("このアプリを真剣に考えてくれたあなたならペットを幸せにできます．")
-        YesButton.setTitle("飼っている人へ", for: .normal)
-        NoButton.setTitle("飼いたい人へ", for: .normal)
+        textshow()
+        YesButton.setTitle("私個人の意見", for: .normal)
+        //NoButton.setTitle("飼いたい人へ", for: .normal)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func have() {
         
     }
-    
-    @IBAction func donthave() {
-        
+    //text 表示
+    func textshow() {
+        thanks.text = String("見てくれてありがとうございます．")
+        text1.text = String("このアプリを真剣に考えてくれたあなたならペットを幸せにできます．知識が足りなかったとしても，このアプリを通して，知るきっかけを提供できたので調べたり，考えてくだされば大丈夫です．生き物を大切にできる方々に悪い方はいません．しかし，最後に一つ考えて欲しいことがあります．")
+        text2.text = String("良い飼い主ってなんですか？")
     }
 
+    //
+    func textsize() {
+        
+    }
     /*
     // MARK: - Navigation
 
